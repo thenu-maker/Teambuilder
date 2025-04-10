4ee3c551ad7fe5d6eed573e104eafcf717bce874
BiberHatFieber <silas.weber@mail.de>
2025-04-10 10:35:31 +0200

    Grundlegendes SpringBoot Projekt

---
+++ src/main/java/com/softwareprojekt/teambuilder/TeambuilderApplication.java
@@ -0,0 +1,13 @@
+package com.softwareprojekt.teambuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeambuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeambuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softwareprojekt/teambuilder/TeambuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwareprojekt.teambuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeambuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
