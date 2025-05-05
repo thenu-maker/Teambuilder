760dbcf805edd7a46322d79dfb4c50b8701d92cc
Sawolga <tol.kil@gmx.de>
2025-05-05 21:00:33 +0200

    Teilnehmer und Veranstaltungsklassen

---
+++ src/main/java/com/softwareprojekt/teambuilder/Entities/Teilnehmer.java
@@ -0,0 +1,51 @@
+package com.softwareprojekt.teambuilder.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+
+@Entity
+public class Teilnehmer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long matrnr;
+
+    private String vorname;
+    private String nachname;
+
+    public Teilnehmer(){
+
+    }
+
+    public long getMatrnr() {
+        return matrnr;
+    }
+
+    public void setMatrnr(long matrnr) {
+        this.matrnr = matrnr;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    @ManyToMany
+    private List<Veranstaltung> veranstaltungen;
+}
---
+++ src/main/java/com/softwareprojekt/teambuilder/Entities/Veranstaltung.java
@@ -0,0 +1,51 @@
+package com.softwareprojekt.teambuilder.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+
+@Entity
+public class Veranstaltung {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String titel;
+    private int semester;
+
+    public Veranstaltung(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    @ManyToMany (mappedBy = "veranstaltungen")
+    private List<Teilnehmer> teilnehmer;
+}
