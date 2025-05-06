0c6bac76472d2d6663389caee5507a8d2559fcad
Thenujan <thenujan.karunakumar@web.de>
2025-05-06 12:10:50 +0200

    Class Gruppe and Teilnahme

--- src/main/java/com/softwareprojekt/teambuilder/Entities/Gruppe.java
+++ src/main/java/com/softwareprojekt/teambuilder/Entities/Gruppe.java
@@ -1,4 +1,42 @@
 package com.softwareprojekt.teambuilder.Entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
+@Entity
 public class Gruppe {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String Gruppenname;
+    private String Anmerkungen;
+
+    public Gruppe(){
+
+    }
+    public long getId() {
+        return id;
+    }
+
+    public String getAnmerkungen() {
+        return Anmerkungen;
+    }
+
+    public void setAnmerkungen(String anmerkungen) {
+        Anmerkungen = anmerkungen;
+    }
+
+    public String getGruppenname() {
+        return Gruppenname;
+    }
+
+    public void setGruppenname(String gruppenname) {
+        Gruppenname = gruppenname;
+    }
 }
---
+++ src/main/java/com/softwareprojekt/teambuilder/Entities/Teilnahme.java
@@ -0,0 +1,58 @@
+package com.softwareprojekt.teambuilder.Entities;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+public class Teilnahme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id; //unsicher
+
+    private double Leistungspunkte;
+    private double Praesentationspunkte;
+
+    @ManyToOne
+    private Gruppe gruppe;
+    @ManyToOne
+    private Teilnehmer teilnehmer;
+
+    public long getId() {
+        return id;
+    }
+
+    public double getPraesentationspunkte() {
+        return Praesentationspunkte;
+    }
+
+    public void setPraesentationspunkte(double praesentationspunkte) {
+        Praesentationspunkte = praesentationspunkte;
+    }
+
+    public double getLeistungspunkte() {
+        return Leistungspunkte;
+    }
+
+    public void setLeistungspunkte(double leistungspunkte) {
+        Leistungspunkte = leistungspunkte;
+    }
+
+    public Gruppe getGruppe() {
+        return gruppe;
+    }
+
+    public void setGruppe(Gruppe gruppe) {
+        this.gruppe = gruppe;
+    }
+
+    public Teilnehmer getTeilnehmer() {
+        return teilnehmer;
+    }
+
+    public void setTeilnehmer(Teilnehmer teilnehmer) {
+        this.teilnehmer = teilnehmer;
+    }
+
+}
